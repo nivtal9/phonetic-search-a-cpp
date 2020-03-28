591b10c7822046c4ed11cabdd83abf052e820ade
avi130 <57074327+avi130@users.noreply.github.com>
2020-03-29 01:01:59 +0300

    Update PhoneticFinder.cpp

--- PhoneticFinder.cpp
+++ PhoneticFinder.cpp
@@ -7,15 +7,7 @@ using namespace std;
 string phonetic::find(string text, string word){
     int i=0;
     int j=0;
-    if(text.length()==1){
-        while(j<text.length() ){
-        if(isSmiliar(text.at(j),word.at(i))){
-            return text.substr(j - i, i+1);
-        }
-            j++;
-        }
-        
-    }
+    
     while(j<text.length()-1 && i<word.length()){
         bool flag=true;
 
