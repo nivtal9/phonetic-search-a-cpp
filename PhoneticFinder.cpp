3bce3c895a7ee06f1a07298a73fbb6983631d0c7
avi <avraham836@gmail.com>
2020-04-01 14:05:32 +0300

    fix

--- PhoneticFinder.cpp
+++ PhoneticFinder.cpp
@@ -17,10 +17,7 @@ string phonetic::find(string text, string word){
                 if(word.length()-1!=i) {
                     i++;
                 }
-                if(word.length()==1 && text.at(j+1)==' '){
-                    return text.substr(j - i, i+1);
-
-                }
+                
                 if(text.length()-1!=j ) {
                     j++;
                 }
